ce13ddd6ffc49fffd4112f2905c3b25bc5e083fc
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-19 01:14:13 +0330

    Change AddCommandHandlers to AddCommandQueryHandlers then add new parameter named commandQueryHandler - add services to program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,10 +1,12 @@
 using Library.Command;
+using Library.Query;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddCommandHandlers(typeof(Program));
+builder.Services.AddCommandQueryHandlers(typeof(Program), typeof(ICommandHandler<>));
+builder.Services.AddCommandQueryHandlers(typeof(Program), typeof(IQueryHandler<,>));
 builder.Services.AddScoped<ICommandBus, CommandBus>();
 
 var app = builder.Build();
